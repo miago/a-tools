c9b114dcb972a9c7249056aa6b00b3a1e53226ae
Miago Cube <miagox@gmail.com>
2012-02-16 00:21:00 +0100

    tolti linee inutili

--- alteratools.sh
+++ alteratools.sh
@@ -4,8 +4,6 @@
 mode="$1"
 DEFAULT_SOPCINFO="/home/miago/zhaw/BA/project/fpga/linsoft.sopcinfo"
 DEFAULT_DTS="/home/miago/zhaw/BA/project/linux/device.dts"
-
-
 DEFAULT_SOF_1="/home/miago/zhaw/BA/project/fpga/linsoft_time_limited.sof"
 DEFAULT_SOF_2="/home/miago/zhaw/BA/project/fpga/linsoft.sof"
 DEFAULT_ZIM="/home/miago/zhaw/BA/project/linux/zImage.initram.gz"
